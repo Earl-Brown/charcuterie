c30df5bf16a0dfb2e5acfe8ed935f7ff3ce7028f
Earl Brown <github@theGleep.com>
2021-11-20 17:22:02 -0700

    Delegate card data aligns well; needs config details shown

--- controls/delegateCard.jsx
+++ controls/delegateCard.jsx
@@ -11,20 +11,22 @@ export function DelegateCard(props) {
 	return <>
 		<div style={{ borderRadius: "15px", backgroundColor: backgroundColor, margin: "1em", padding: "1em" }}>
 			<div>
-				<div style={{ float: "right", verticalAlign: "top", top: "-0.25em", position: "relative" }}><ConfigButton /></div>
+				<div style={{ float: "right", verticalAlign: "middle", top: "-0.25em", position: "relative" }}><ConfigButton /></div>
 				<h2 style={{ margin: 0, padding: 0, borderBottom: "0.1em solid black" }}>{validator.name}</h2>
 			</div>
 			<Panel title="Commission" titleBackground="pink" style={{ border: "2px solid brown" }} titleStyle={{ backgroundColor: backgroundColor, opacity: "1" }}>
-				<div>Current: {(validator.rate * 100).toFixed(3)}%</div>
-				<div>Max Delta: {(validator["max-change-rate"] * 100).toFixed(3)}%</div>
-				<div>Max Rate: {(validator["max-rate"] * 100).toFixed(3)}%</div>
-			</Panel>
-			<Panel title="APR" style={{ border: "2px solid brown" }} titleStyle={{ backgroundColor: backgroundColor, opacity: "1" }}>
-				<div>
-					<div>Current: {(validator.apr * 100).toFixed(3)}%</div>
-					<div>Last: {(validator.last_apr * 100).toFixed(3)}%</div>
+				<div style={{ display: "grid", columnGap: "0.75em", gridTemplateAreas: "'label1 data1' 'label2 data2' 'label3 data3'" }}>
+					<div style={{ gridArea: 'label1' }}>Current:</div><div style={{ gridArea: "data1" }}>{(validator.rate * 100).toFixed(3)}%</div>
+					<div style={{ gridArea: 'label2' }}>Max change:</div><div style={{ gridArea: "data2" }}>{(validator["max-change-rate"] * 100).toFixed(3)}%</div>
+					<div style={{ gridArea: 'label3' }}>Max change rate:</div><div style={{ gridArea: "data3" }}>{(validator["max-rate"] * 100).toFixed(3)}%</div>
 				</div>
 			</Panel>
-		</div>
+			<Panel title="APR" style={{ border: "2px solid brown" }} titleStyle={{ backgroundColor: backgroundColor, opacity: "1" }}>
+				<div style={{ display: "grid", columnGap: "0.75em", gridTemplateAreas: "'label1 data1' 'label2 data2'" }}>
+					<div style={{ gridArea: "label1" }}>Current: </div> <div style={{ gridArea: "data1" }}>{(validator.apr * 100).toFixed(3)}%</div>
+					<div style={{ gridArea: "label2" }}>Last: </div> <div style={{ gridArea: "data2" }}>{(validator.last_apr * 100).toFixed(3)}%</div>
+				</div>
+			</Panel >
+		</div >
 	</>
 }
