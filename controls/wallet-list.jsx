37bf947c6bdccdfd2570c69473be1158c4f45f3d
Earl Brown <github@theGleep.com>
2021-12-10 22:58:45 -0700

    Wallet list is looking nice now.

--- controls/wallet-list.jsx
+++ controls/wallet-list.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react"
 import { AddWallet, RemoveWallet } from "../services/appstateservice"
-import { ActionIcon, Paper, Title, Group } from "@mantine/core"
+import { TextInput, ActionIcon, Paper, Title, Group } from "@mantine/core"
 import { FaTrashAlt } from 'react-icons/fa'
 
 export const WalletList = props => {
 	const { wallets, onChange } = props
+	const [currentInput, setInput] = useState("")
 
 	const updateWallets = list => {
 		console.log("Updating list", list)
@@ -13,13 +14,26 @@ export const WalletList = props => {
 
 	const addWallet = async id => {
 		const list = await AddWallet(id)
+		setInput("")
 		updateWallets(list)
+	}
+
+	const addWalletIfInputComplete = async event => {
+		if (
+			event.key !== "Enter"
+		) return
+		const id = event.target.value
+		if (id === "") return
+
+		addWallet(id)
 	}
 
 	const removeWallet = async id => {
 		const list = await RemoveWallet(id)
 		updateWallets(list)
 	}
+
+	const noop = _ => { }
 
 	const Wallet = address => <Group key={address} style={{ fontSize: "larger" }}>
 		<ActionIcon style={{ fontSize: "large" }} onClick={e => removeWallet(address)} ><FaTrashAlt /></ActionIcon>
@@ -30,10 +44,17 @@ export const WalletList = props => {
 		<Paper padding="sm" shadow="xs" radius={0} style={{ backgroundColor: "#DDEFEFDD" }}>
 			<Title order={3} style={{ width: "100%", borderBottom: "1px solid black", paddingBottom: 0, marginBottom: "0.25em", }}>Wallets</Title>
 			{wallets.map((a, idx) => Wallet(a))}
-			<input
-				style={{ border: "1px solid silver", marginTop: "0.2em", marginLeft: "0", paddingLeft: "0", position: "relative", left: "-0.1em", backgroundColor: "#EEFFEE", fontSize: "larger", width: "30em" }}
-				onChange={e => addWallet(e.currentTarget.value)}
+
+			<TextInput
+				placeholder="Add a wallet, hit ENTER when you're done"
+				size="lg"
+				value={currentInput}
+				onChange={e => setInput(e.target.value)}
+				onKeyUp={e => addWalletIfInputComplete(e)}
+				styles={{ input: { fontSize: "x-large" } }}
+				wrapperProps={{ style: { marginTop: "0.75em" } }}
 			/>
+
 		</Paper>
 	</>
 }
